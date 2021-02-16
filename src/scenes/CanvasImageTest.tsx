60927fb04b5648484e9791ff7a7633cca350d495
AdrianCryer <adrian.cryer3@gmail.com>
2021-02-17 01:51:10 +1000

    Added 4D noise and generated tiled 2d textures

--- src/scenes/CanvasImageTest.tsx
+++ src/scenes/CanvasImageTest.tsx
@@ -5,7 +5,6 @@ let { Noise } = require('noisejs');
 export const CanvasImageTest = (props: any) => {
     const ref = useRef<HTMLCanvasElement>(null);
     
-    
     useEffect(() => {
 
         const canvas = ref.current;
@@ -17,25 +16,43 @@ export const CanvasImageTest = (props: any) => {
         context.imageSmoothingEnabled = false;
         let image = context.createImageData(canvas.width, canvas.height);
         let data = image.data;
-
         const gen = new PerlinNoiseGenerator(1000);
         const noise = new Noise(1000);
 
-        for (let x = 0; x < canvas.width; x++) {
-            for (let y = 0; y < canvas.height; y++) {
+        let offset = 0;
+        let frame = -1;
+        const tilingPeriod = 2;
+        const scale = 0.5;
 
-                // let value = 255 * Math.random();
-                // let value = 256 * gen.generate2D(x / 5, y / 5);
-                console.log(gen.generate2D(x / 5, y / 5))
-                let value =  128 * (1 + gen.generate2D(x / 5, y / 5));
-                // let value = 128 * (1 + noise.perlin2(x / 5, y / 5));
-
-                let cell = (x + y * canvas.width) * 4;;
-                data[cell] = data[cell + 1] = data[cell + 2] = value;
-                data[cell + 3] = 255;
+        function renderLoop() {
+            if (canvas === null || !context) {
+                return;
             }
+            for (let x = 0; x < canvas.width; x++) {
+                for (let y = 0; y < canvas.height; y++) {
+    
+                    let mx = Math.cos(x / canvas.width * 2 * Math.PI * tilingPeriod);
+                    let my = Math.sin(x / canvas.width * 2 * Math.PI * tilingPeriod);
+                    let mz = Math.cos(y / canvas.height * 2 * Math.PI * tilingPeriod);
+                    let mw = Math.sin(y / canvas.height * 2 * Math.PI * tilingPeriod);
+
+                    let value =  128 * (1 + gen.generate4D(mx * scale + offset, my * scale + offset, mz * scale + offset, mw * scale + offset));
+                    // let value = 128 * (1 + noise.perlin2(x / 20, y / 20));
+                    if (value > 128)
+                        value /= 1.2
+    
+                    let cell = (x + y * canvas.width) * 4;;
+                    data[cell] = data[cell + 1] = data[cell + 2] = value;
+                    data[cell + 3] = 255;
+                }
+            }
+            context.putImageData(image, 0, 0);
+            offset += 0.01;
+            frame = requestAnimationFrame(renderLoop)
         }
-        context.putImageData(image, 0, 0);
+        renderLoop()
+
+        return () => cancelAnimationFrame(frame)
     }, [])
 
     const style = {
@@ -43,5 +60,5 @@ export const CanvasImageTest = (props: any) => {
         height: 800,
     };
 
-    return <canvas width={64} height={64} style={{...style, imageRendering: 'pixelated' }} ref={ref}/>;
+    return <canvas width={128} height={128} style={{...style, imageRendering: 'pixelated' }} ref={ref}/>;
 }
